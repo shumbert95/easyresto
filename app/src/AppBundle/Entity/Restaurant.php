c71fdae58b0a89118f0bdf43444240cd629d7756
simon humbert <humbertsimon@gmail.com>
2018-07-04 12:29:59 +0200

    Add getter/setter for longitude/latitude in entity restaurant

--- app/src/AppBundle/Entity/Restaurant.php
+++ app/src/AppBundle/Entity/Restaurant.php
@@ -471,6 +471,37 @@ class Restaurant
         $this->averageNote = $averageNote;
     }
 
+    /**
+     * @return float
+     */
+    public function getLatitude()
+    {
+        return $this->latitude;
+    }
+
+    /**
+     * @param float $latitude
+     */
+    public function setLatitude($latitude)
+    {
+        $this->latitude = $latitude;
+    }
+
+    /**
+     * @return float
+     */
+    public function getLongitude()
+    {
+        return $this->longitude;
+    }
+
+    /**
+     * @param float $longitude
+     */
+    public function setLongitude($longitude)
+    {
+        $this->longitude = $longitude;
+    }
 
     public function __toString()
     {
